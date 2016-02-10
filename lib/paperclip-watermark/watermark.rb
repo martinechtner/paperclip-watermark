69388c2b525093537badbff4dd2b9a656b0e3b2c
martinechtner <martin.echtner@gmail.com>
2016-02-10 12:25:53 -0700

    add the ability to set an offset for the watermark

--- lib/paperclip-watermark/watermark.rb
+++ lib/paperclip-watermark/watermark.rb
@@ -1,6 +1,6 @@
 # from http://github.com/ng/paperclip-watermarking-app
 # with modifications from http://exviva.posterous.com/watermarking-images-with-rails-3-and-papercli
-# and even more modifications to ensure works with paperclip >= 2.3.8 and rails >= 3
+# and even more modifications to ensure works with paperclip >= 4.3.5 and rails >= 4.2.5.1
 #
 # Note: In rails 3 paperclip processors are not automatically loaded.
 # You must add the following above your model class definition:
@@ -10,22 +10,22 @@
 module Paperclip
   class Watermark < Processor
     # Handles watermarking of images that are uploaded.
-    attr_accessor :current_geometry, :target_geometry, :format, :whiny, :convert_options, :watermark_path, :overlay, :position
+    attr_accessor :current_geometry, :target_geometry, :format, :whiny, :convert_options,
+                  :watermark_path, :overlay, :position, :watermark_offset
 
-    def initialize file, options = {}, attachment = nil
+    def initialize(file, options = {}, attachment = nil)
       super
       geometry          = options[:geometry]
       @file             = file
-      if geometry.present?
-        @crop             = geometry[-1,1] == '#'
-      end
+      @crop             = geometry[-1,1] == '#' if geometry.present?
       @target_geometry  = Geometry.parse geometry
       @current_geometry = Geometry.from_file @file
       @convert_options  = options[:convert_options]
       @whiny            = options[:whiny].nil? ? true : options[:whiny]
       @format           = options[:format]
       @watermark_path   = options[:watermark_path]
-      @position         = options[:position].nil? ? "SouthEast" : options[:position]
+      @position         = options[:position].nil? ? 'SouthEast' : options[:position]
+      @watermark_offset = options[:watermark_offset].nil? ? '+0+0' : options[:watermark_offset]
       @overlay          = options[:overlay].nil? ? true : false
       @current_format   = File.extname(@file.path)
       @basename         = File.basename(@file.path, @current_format)
@@ -46,25 +46,26 @@ module Paperclip
     # Performs the conversion of the +file+ into a watermark. Returns the Tempfile
     # that contains the new image.
     def make
-      dst = Tempfile.new([@basename, @format].compact.join("."))
+      dst = Tempfile.new([@basename, @format].compact.join('.'))
       dst.binmode
 
-      command = "convert"
+      command = 'convert'
       params = [fromfile]
       params += transformation_command
       params << tofile(dst)
       begin
-        success = Paperclip.run(command, params.flatten.compact.collect{|e| "'#{e}'"}.join(" "))
+        success = Paperclip.run(command, params.flatten.compact.collect{|e| "'#{e}'"}.join(' '))
       rescue Paperclip::Errors::CommandNotFoundError
         raise Paperclip::Errors::CommandNotFoundError, "There was an error resizing and cropping #{@basename}" if @whiny
       end
 
       if watermark_path
-        command = "composite"
+        command = 'composite'
         params = %W[-gravity #{@position} #{watermark_path} #{tofile(dst)}]
+        params += %W[-geometry #{@watermark_offset}]
         params << tofile(dst)
         begin
-          success = Paperclip.run(command, params.flatten.compact.collect{|e| "'#{e}'"}.join(" "))
+          success = Paperclip.run(command, params.flatten.compact.collect{|e| "'#{e}'"}.join(' '))
         rescue Paperclip::Errors::CommandNotFoundError
           raise Paperclip::Errors::CommandNotFoundError, "There was an error processing the watermark for #{@basename}" if @whiny
         end
--- paperclip-watermark.gemspec
+++ paperclip-watermark.gemspec
@@ -1,24 +1,23 @@
-# -*- encoding: utf-8 -*-
 lib = File.expand_path('../lib', __FILE__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 require 'paperclip-watermark/version'
 
 Gem::Specification.new do |gem|
-  gem.name          = "paperclip-watermark"
+  gem.name          = 'paperclip-watermark'
   gem.version       = PaperclipWatermark::VERSION
-  gem.authors       = ["Vik Ewoods"]
-  gem.email         = ["support@voroninstudio.eu"]
+  gem.authors       = ['Vik Ewoods', 'Martin Echtner']
+  gem.email         = ['martin.echtner@gmail.com']
   gem.description   = %q{Paperclip Watermark processor}
   gem.summary       = %q{Paperclip Watermark processor}
-  gem.homepage      = "http://vikewoods.com/rails/paperclip-watermark"
+  gem.homepage      = 'https://github.com/martinechtner/paperclip-watermark'
 
   gem.files         = `git ls-files`.split($/)
   gem.executables   = gem.files.grep(%r{^bin/}).map{ |f| File.basename(f) }
   gem.test_files    = gem.files.grep(%r{^(test|spec|features)/})
-  gem.require_paths = ["lib"]
-  gem.add_dependency "rake"
-  gem.add_dependency "rails"
-  gem.add_dependency "minitest"
-  gem.add_dependency "minitest-reporters"
-  gem.add_dependency "paperclip", ">= 2.3.8"
+  gem.require_paths = ['lib']
+  gem.add_dependency 'rake'
+  gem.add_dependency 'rails'
+  gem.add_dependency 'minitest'
+  gem.add_dependency 'minitest-reporters'
+  gem.add_dependency 'paperclip', '>= 2.3.8'
 end
